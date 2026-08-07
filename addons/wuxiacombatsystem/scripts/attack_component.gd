6b8f66d96d1d58a97331ec035300fa2a32f235cc
Zten334 <2811350013@qq.com>
2026-08-07 14:13:21 +0800

    V0.1Alpha
    新增了attackcomponent中的攻击信息相关参数，使其职责与resourcecomponent分离，二者关系更为清晰
    attackcomponent负责在游戏中进行判定
    resourcecomponent负责在游戏加载阶段将数据写入游戏中

--- addons/wuxiacombatsystem/scripts/attack_component.gd
+++ addons/wuxiacombatsystem/scripts/attack_component.gd
@@ -1,22 +1,64 @@
 extends Area3D
 
-@export var init_scale : Vector3
+#攻击数据的基础信息
+class AbilityInfo:
+	
+	var total_duration : float #总持续时长
+	
+	var front_time : float #前摇结束的时间点，running结束的时间点
+	var running_time : float
+	
+	var hit_check_points : Array[float] #攻击检测点
+
+#region HITCHECK_BOX
+@export var init_scale : Vector3  #测试用的初始碰撞盒scale
 
 @onready var collision : DynamicBoxCollision = $DynamicBoxCollision
+#endregion
+#region ATTACKINFO
 
+var normal_attack_info : Array[AbilityInfo] #普通攻击的所有派生数组
+var ability1_info : Array[AbilityInfo]  #能力一的所有派生
+var ability2_info : Array[AbilityInfo]  #能力二的所有派生
+var ability3_info : Array[AbilityInfo]  #能力三的所有派生
+
+var normal_attack_index : int #普通攻击当前的派生顺序
+var ability1_index : int      #能力一的派生顺序
+var ability2_index : int      #能力二的派生顺序
+var ability3_index : int      #能力三的派生顺序
+
+#endregion
 func _ready() -> void:
 	init_collision(init_scale) 
 
 func _process(delta: float) -> void:
 	pass
 
-#region EXCUTE_ATTACKING
+#region INITRESOURCE
+#只在进入场景时执行一次，初始化攻击内容
+func init_ability_info(resource) -> void:
+	#初始化一个ability_info
+	var ability_info = AbilityInfo.new()
+	ability_info.total_duration = 0.8
+	ability_info.front_time = 0.1
+	ability_info.running_time = 0.6
+	#加入到数组中
+	#abilities.append(ability_info)
+	
+	
+
 #初始化碰撞大小
 func init_collision(target_scale : Vector3) -> void:
 	if not collision:
 		return
 	#设置碰撞盒的尺寸
 	collision.set_shape_scale(target_scale)
+
+#endregion
+
+
+#region EXCUTE_ATTACKING
+
 
 func excute_attack(skill_name : StringName)-> void:
 	pass
